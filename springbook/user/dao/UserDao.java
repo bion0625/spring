b5a0a81be6bcb5290bf6577673b90ab6c32d4cfe
bion0625 <stxtory@gmail.com>
2024-03-31 13:17:18 +0900

    1.3.2 인터페이스의 도입

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -9,14 +9,14 @@ import springbook.user.domain.User;
 
 public class UserDao {
 
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker; // 인터페이스를 통해 오브젝트에 접근하므로 구체적인 클래스 정보를 알 필요가 없다.
 
     public UserDao() {
-        simpleConnectionMaker = new SimpleConnectionMaker();
+        connectionMaker = new DConnectionMaker(); // 앗! 그런데 여기에는 클래스 이름이 나오네!!
     }
 
     public void deleteAll() throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNew();
+        Connection c = connectionMaker.makeConnection(); // 인터페이스에 정의된 메소드를 사용하므로 클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다.
         PreparedStatement ps = c.prepareStatement(
             "delete from users");
 
@@ -27,7 +27,7 @@ public class UserDao {
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNew();
+        Connection c = connectionMaker.makeConnection(); // 인터페이스에 정의된 메소드를 사용하므로 클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다.
         
         PreparedStatement ps = c.prepareStatement(
             "insert into users(id, name, password) values(?,?,?)");
@@ -42,7 +42,7 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNew();
+        Connection c = connectionMaker.makeConnection(); // 인터페이스에 정의된 메소드를 사용하므로 클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다.
 
         PreparedStatement ps = c.prepareStatement(
             "select * from users where id = ?");
