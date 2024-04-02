8a0869999039678064e217da8a62fee47dfef9d4
bion0625 <bion0625@gmail.com>
2024-04-03 08:32:35 +0900

    1.7.5 메소드를 이용한 의존관계 주입

--- springbook/user/dao/DaoFactory.java
+++ springbook/user/dao/DaoFactory.java
@@ -1,21 +1,23 @@
 package springbook.user.dao;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
+@Configuration // 애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
     @Bean // 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
     public UserDao userDao() {
         ConnectionMaker connectionMaker = connectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker);
         return userDao;
     }
 
     @Bean
     public UserDao specialUserDao() {
         ConnectionMaker connectionMaker = connectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao();
+        userDao.setConnectionMaker(connectionMaker);
         return userDao;
     }
 
--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -11,7 +11,7 @@ public class UserDao {
 
     private ConnectionMaker connectionMaker; // 인터페이스를 통해 오브젝트에 접근하므로 구체적인 클래스 정보를 알 필요가 없다.
 
-    public UserDao(ConnectionMaker connectionMaker) {
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
