73640ad6e6c1b4bcb923644f918cd94da4bc5e9e
bion0625 <bion0625@gmail.com>
2024-04-04 10:14:40 +0900

    2.2.2 테스트의 효율적인 수행과 관리

--- springbook/user/dao/userDaoTest.java
+++ springbook/user/dao/userDaoTest.java
@@ -1,18 +1,29 @@
 package springbook.user.dao;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 import java.sql.SQLException;
 
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.domain.User;
 
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException {
+
+    public static void main(String[] args) {
+        JUnitCore.main("springbook.user.dao.UserDaoTest");
+    }
+
+    @Test // JUnit에게 테스트용 메소드임을 알려준다.
+    public void addAndGet() throws SQLException { // JUnit 메소드는 반드시 public으로 선언되어야 한다.
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("specialUserDao", UserDao.class);
-        
+
         dao.deleteAll();
 
         User user = new User();
@@ -25,13 +36,8 @@ public class UserDaoTest {
         System.out.println(user.getId() + " 등록 성공");
 
         User user2 = dao.get(user.getId());
-        
-        if (!user.getName().equals(user2.getName())) {
-            System.out.println("테스트 실패 (name)");
-        } else if (!user.getPassword().equals(user2.getPassword())) {
-            System.out.println("테스트 실패 (password)");
-        } else {
-            System.out.println("조회 테스트 성공");
-        }
+
+        assertThat(user2.getName(), is(user.getName()));
+        assertThat(user2.getPassword(), is(user.getPassword()));
     }
 }
