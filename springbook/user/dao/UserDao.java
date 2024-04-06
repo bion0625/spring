a8957bd769f9309720288eb0377ffbe9c04e0d7f
이의정 <stxtory@gmail.com>
2024-04-06 22:16:39 +0900

    3.1.1 예외처리 기능을 갖춘 DAO

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -26,14 +26,36 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        Connection c = dataSource.getConnection(); // 인터페이스에 정의된 메소드를 사용하므로 클래스가 바뀐다고 해도 메소드 이름이 변경될 걱정은 없다.
-        PreparedStatement ps = c.prepareStatement(
-            "delete from users");
+        Connection c = null;
+        PreparedStatement ps = null;
 
-        ps.executeUpdate();
+        try { // 예외가 발생할 가능성이 있는 코드를 모두 try 블록으로 묶어준다.
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) { // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다.
+            throw e; // 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
+        } finally { // finally 이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
+            if (ps != null) {
+                try{
+                    ps.close();
+                } catch(SQLException e) {
+                    /*
+                     * ps.close() 메소드에서도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
+                     * 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져나갈 수 있다.
+                    */
+                }
+            }
+            if (c != null) {
+                try{
+                    c.close(); // Connection 반환
+                } catch(SQLException e) {
+                }
+            }
+            ps.close();
+            c.close();
+        }
 
-        ps.close();
-        c.close();
     }
 
     public void add(User user) throws SQLException {
@@ -78,16 +100,41 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
+        try {
+            c = dataSource.getConnection();
+            ps = c.prepareStatement("select count(*) from users");
 
-        rs.close();
-        ps.close();
-        c.close();
+            // resultSet도 다양한 SQLException이 발생할 수 있는 코드이므로 try 블록 안에 둬야 한다.
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close(); // 만들어진 resultSet을 닫아주는 기능. close()는 만들어진 순서의 반대로 하는 것이 원칙이다.
+                } catch (SQLException e) {
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+
 
         return count;
     }
