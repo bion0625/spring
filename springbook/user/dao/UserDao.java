855d7186ee655c494c72b9df17ba2a1f0fb18679
이의정 <stxtory@gmail.com>
2024-04-08 20:42:49 +0900

    3.3.1 전략 클래스의 추가 정보

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -31,39 +31,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try { // 예외가 발생할 가능성이 있는 코드를 모두 try 블록으로 묶어준다.
-            c = dataSource.getConnection();
-            ps = c.prepareStatement(
-            "insert into users(id, name, password) values(?,?,?)");
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-            ps.executeUpdate();
-        } catch (SQLException e) { // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다.
-            throw e; // 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
-        } finally { // finally 이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
-            if (ps != null) {
-                try{
-                    ps.close();
-                } catch(SQLException e) {
-                    /*
-                     * ps.close() 메소드에서도 SQLException이 발생할 수 있기 때문에 이를 잡아줘야 한다.
-                     * 그렇지 않으면 Connection을 close() 하지 못하고 메소드를 빠져나갈 수 있다.
-                    */
-                }
-            }
-            if (c != null) {
-                try{
-                    c.close(); // Connection 반환
-                } catch(SQLException e) {
-                }
-            }
-            ps.close();
-            c.close();
-        }
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
