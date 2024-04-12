337202452a163fb9246c3a9ba3d157ac03c8d0dd
bion0625 <stxtory@gmail.com>
2024-04-12 19:18:44 +0900

    3.6.2 queryForInt()

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -9,7 +9,6 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 
 import springbook.user.domain.User;
 
@@ -69,39 +68,6 @@ public class UserDao {
     }
 
     public int getCount() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-
-            // resultSet도 다양한 SQLException이 발생할 수 있는 코드이므로 try 블록 안에 둬야 한다.
-            rs = ps.executeQuery();
-            rs.next();
-            return rs.getInt(1);
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close(); // 만들어진 resultSet을 닫아주는 기능. close()는 만들어진 순서의 반대로 하는 것이 원칙이다.
-                } catch (SQLException e) {
-                }
-            }
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
+        return this.jdbcTemplate.queryForInt("select count(*) from users");
     }
 }
