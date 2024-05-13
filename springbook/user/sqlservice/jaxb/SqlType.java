47fd8eed468904906fcf83938dfb31b205a602e4
bion0625 <bion0625@gmail.com>
2024-05-13 15:22:06 +0900

    7.2.1 XML 파일 매핑

---
+++ springbook/user/sqlservice/jaxb/SqlType.java
@@ -0,0 +1,94 @@
+//
+// �� ������ JAXB(JavaTM Architecture for XML Binding) ���� ���� 2.2.8-b130911.1802 ������ ���� �����Ǿ����ϴ�. 
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>�� �����Ͻʽÿ�. 
+// �� ������ �����ϸ� �ҽ� ��Ű���� ���������� �� ���� ������ �սǵ˴ϴ�. 
+// ���� ��¥: 2024.05.13 �ð� 02:02:06 PM KST 
+//
+
+
+package springbook.user.sqlservice.jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+
+/**
+ * <p>sqlType complex type�� ���� Java Ŭ�����Դϴ�.
+ * 
+ * <p>���� ��Ű�� ������ �� Ŭ������ ���ԵǴ� �ʿ��� �������� �����մϴ�.
+ * 
+ * <pre>
+ * &lt;complexType name="sqlType">
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
+ *       &lt;attribute name="key" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "sqlType", propOrder = {
+    "value"
+})
+public class SqlType { // <sql> 태그 한 개당 SqlType 오브젝트가 하나씩 만들어진다.
+
+    @XmlValue
+    protected String value; // SQL 값을 저장할 스트링 타입의 필드
+    @XmlAttribute(name = "key", required = true)
+    protected String key; // KEY 애트리뷰트에 담긴 검색용 키 값을 위한 스트링 타입의 필드
+
+    /**
+     * value �Ӽ��� ���� �����ɴϴ�.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * value �Ӽ��� ���� �����մϴ�.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /**
+     * key �Ӽ��� ���� �����ɴϴ�.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * key �Ӽ��� ���� �����մϴ�.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setKey(String value) {
+        this.key = value;
+    }
+
+}
