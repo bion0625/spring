51a2c6355ae9d64b30e974bebea21509b77666b4
bion0625 <bion0625@gmail.com>
2024-05-21 08:59:36 +0900

    7.5.1 ConcurrentHashMap을 이용한 수정 기능 SQL 레지스트리

---
+++ springbook/user/sqlservice/updatable/ConcurrentHashMapSqlRegistry.java
@@ -0,0 +1,39 @@
+package springbook.user.sqlservice.updatable;
+
+import springbook.issuetracker.sqlservice.SqlUpdateFailureException;
+import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
+import springbook.user.sqlservice.SqlNotFountException;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapSqlRegistry implements UpdatableSqlRegistry {
+
+    Map<String, String> sqlMap = new ConcurrentHashMap<>();
+
+    @Override
+    public String findSql(String key) throws SqlNotFountException {
+        String sql = sqlMap.get(key);
+        if (sql == null) throw new SqlNotFountException(String.format("%s에 해당하는 SQL을 찾을 수 없습니다.", key));
+        else return sql;
+    }
+
+    @Override
+    public void registerSql(String key, String sql) { sqlMap.put(key, sql); }
+
+    @Override
+    public void updateSql(String key, String sql) throws SqlUpdateFailureException {
+        if (sqlMap.get(key) == null) {
+            throw new SqlUpdateFailureException(String.format("%s에 해당하는 SQL을 찾을 수 없습니다", key));
+        }
+
+        sqlMap.put(key, sql);
+    }
+
+    @Override
+    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
+        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+            updateSql(entry.getKey(), entry.getValue());
+        }
+    }
+}
