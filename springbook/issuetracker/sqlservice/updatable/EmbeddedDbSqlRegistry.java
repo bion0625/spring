41c7100a120e986f3b806b54e42d2103c72f5e25
bion0625 <bion0625@gmail.com>
2024-05-21 13:00:25 +0900

    7.5.3 트랜젝션 적용

--- springbook/issuetracker/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ springbook/issuetracker/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -2,6 +2,10 @@ package springbook.issuetracker.sqlservice.updatable;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 import springbook.issuetracker.sqlservice.SqlUpdateFailureException;
 import springbook.issuetracker.sqlservice.UpdatableSqlRegistry;
 import springbook.user.sqlservice.SqlNotFountException;
@@ -12,9 +16,18 @@ import java.util.Map;
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     SimpleJdbcTemplate jdbc;
 
+    /*
+    * JdbcTemplate과 트랜젝션을 동기화해주는 트랜젝션 템플릿이다.
+    * 멀티 스레드 환경에서 공유가 가능하다.
+    * */
+    TransactionTemplate transactionTemplate;
+
     // DataSource를 DI 받아서 SimpleJdbcTemplate 형태로 저장해두고 사용한다.
     public void setDataSource(DataSource dataSource) {
         jdbc = new SimpleJdbcTemplate(dataSource);
+        transactionTemplate = new TransactionTemplate(
+                // dataSource로 transactionManager를 만들고 이를 이용해 TransactionTemplate을 생성한다.
+                new DataSourceTransactionManager(dataSource));
     }
 
     @Override
@@ -44,9 +57,16 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
     }
 
     @Override
-    public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
-            updateSql(entry.getKey(), entry.getValue());
-        }
+    // 익명 내부 클래스로 만들어지는 콜백 오브젝트 안에서 사용되는 것이라 final로 선언해줘야 한다.
+    public void updateSql(final Map<String, String> sqlmap) throws SqlUpdateFailureException {
+        // 트랜젝션 템플릿이 만드는 트랜젝션 경계 안에서 동작할 코드를 콜백 형태로 만들고 TransactionTemplate의 execute() 메소드에 전달한다.
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
     }
 }
